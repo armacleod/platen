f3c19313c6187ca7a725c9464d7448066ffd7344
armacleod <14943959+armacleod@users.noreply.github.com>
2026-09-20 08:10:00 -0400

    Add tokens and design spec

---
+++ src/lib/index.js
@@ -0,0 +1,4 @@
+export { default as Button } from './Button.svelte';
+export { default as Field } from './Field.svelte';
+export { default as Panel } from './Panel.svelte';
+export { default as Rule } from './Rule.svelte';
